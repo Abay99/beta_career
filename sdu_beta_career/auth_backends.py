6937b5681c65308cee3ba5af6465524859d1a8dc
Yernar Kambar <ernar.k.20@gmail.com>
2019-04-06 12:42:34 +0600

    some changes in auth_backends.py

--- sdu_beta_career/auth_backends.py
+++ sdu_beta_career/auth_backends.py
@@ -1,28 +1,33 @@
 # import the User object
 from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
 
 
 class SDUPortalAuthenticationBackend:
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(self, username: str=None, password: str=None) -> 'Optional[User]':
         """
-        Try to find user in SDU portal database and if that user exists, then to create in SBC database
+        Try to find and authenticate user in SBC database, and if that user doesn't exist, then
+        try to find user in SDU portal database and if that user exists, then to create in SBC database
         :param username: Username or maybe email of user
-        :type username: str
-
         :param password: password of user
-        :type password: str or maybe encrypted str
-
         :return: user from SDU portal database
-        :rtype: instance of a class
         """
         try:
+            #At first try to find user in SBC database
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            else:
+                return None
+        except User.DoesNotExists:
+            #Try to find user in SDU portal database
             if username == username and password == password:
-                user = User.objects.create_user(username=username, password=password)
-                user.first_name = 'TestFirstName'
-                user.last_name = 'TestLastName'
+                user = User.objects.create_user(username=username,
+                                                password=password,
+                                                first_name='TestFirstName',
+                                                last_name='TestLastName')
                 user.save()
                 return user
-        except User.DoesNotExists:
-            # No user was found, return None - triggers default login failed
-            return None
+            else:
+                return None
